c6891ce1f6f14cf1faaab52a3a0305e915ce8ce3
vicRuas <106564712+vicRuas@users.noreply.github.com>
2022-05-30 21:23:24 -0300

    Add files via upload

---
+++ equipamentos.js
@@ -0,0 +1,14 @@
+// JavaScript code
+function search_equip() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase()
+  let x = document.getElementsByClassName('equip')
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = 'none'
+    } else {
+      x[i].style.display = 'list-item'
+    }
+  }
+}
